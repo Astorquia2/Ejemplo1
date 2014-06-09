81c58a3dbd77df5a82e3570110bbd834c986289a
Astorquia2014 <astorquia2014@yahoo.es>
2014-06-09 22:59:02 +0200

    sdsds

---
+++ src/Ejemplo1.java
@@ -0,0 +1,24 @@
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Oscar
+ */
+public class Ejemplo1
+{
+   private int iNum;
+    public static void main (String[] args)
+    {
+        int [][] dos;
+        int num = 5; //número cualquiera cuyo cuadrado
+        //se quiere calcular
+        int result; //variable donde vamos a almacenar
+        //el resultado de la operación
+        result = num * num;
+        System.out.println("El cuadrado de "+num +" es "+result);
+    }
+}
